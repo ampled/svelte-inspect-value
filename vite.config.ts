49647387c327803772c9c5a918e041d0282d1318
ampled <korsneseirik@gmail.com>
2025-01-13 15:23:49 +0100

    disable terser

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [sveltekit()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      keep_classnames: true,
-    },
-  },
+  // build: {
+  //   minify: 'terser',
+  //   terserOptions: {
+  //     keep_classnames: true,
+  //   },
+  // },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
