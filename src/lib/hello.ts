737d56446c1b05984bd34f83534981ee5329dd76
ampled <korsneseirik@gmail.com>
2025-01-22 14:28:57 +0100

    handle console logging differently based on type

--- src/lib/hello.ts
+++ src/lib/hello.ts
@@ -1,13 +1,18 @@
 import type { KeyType } from './types.js'
 import { stringifyOrToString } from './util.js'
 
-export function logToConsole(path: KeyType[] = [], ...items: unknown[]) {
-  console.log(
+export function logToConsole(path: KeyType[] = [], value: unknown, type: string | undefined) {
+  const prefix = [
     `%cInspect ${path.join('.')}`,
     'background-color: black; padding: 5px; color: hotpink; border-radius: 8px;',
-    '\n',
-    ...items
-  )
+  ]
+
+  if (type === 'object') {
+    console.log(...prefix)
+    console.dir(value)
+  } else {
+    console.log(...prefix, '\n', value)
+  }
   // console.dirxml(...items);
 }
 
