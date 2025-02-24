d52649c0e6f0243c322319f5b020576fe7cfdca0
ampled <korsneseirik@gmail.com>
2025-02-24 16:20:32 +0100

    feat: export InspectOptions type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,11 @@ export { default as CustomExpandable } from './CustomExpandable.svelte'
 export { default as CustomLine } from './CustomLine.svelte'
 export { default as Inspect } from './Inspect.svelte'
 export { default as InsectOptionsProvider } from './InspectOptionsProvider.svelte'
-export { GLOBAL_OPTIONS_CONTEXT, setGlobalInspectOptions } from './options.svelte.js'
+export {
+  GLOBAL_OPTIONS_CONTEXT,
+  setGlobalInspectOptions,
+  type InspectOptions,
+} from './options.svelte.js'
 export type { CustomComponentProps, CustomComponents } from './types.js'
 export { addComponent } from './util.js'
 import Inspect from './Inspect.svelte'
