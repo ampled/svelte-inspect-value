78c79f084cbed1e55f75f4f151d6e046a4a63a2a
ampled <korsneseirik@gmail.com>
2025-03-02 14:56:57 +0100

    test

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config()
+
+console.log(process.env)
+
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ['main', { name: 'next', prerelease: true }],
+  branches: [
+    'main',
+    { name: 'release-setup-versioning', channel: 'foo', prerelease: true },
+    { name: 'next', prerelease: true },
+  ],
   repositoryUrl: 'https://github.com/ampled/svelte-inspect-value.git',
 }
