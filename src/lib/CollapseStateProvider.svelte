d40cb0dbdd6ec7dd2e95ca45fb37d0304ed58ee2
ampled <korsneseirik@gmail.com>
2025-04-20 20:27:00 +0200

    provider for collapse state

---
+++ src/lib/CollapseStateProvider.svelte
@@ -0,0 +1,20 @@
+<!--
+@component CollapseStateProvider
+
+Initializes collapse state
+-->
+<script lang="ts">
+  import { setContext, type Snippet } from 'svelte'
+  import type { InspectOptions } from './options.svelte.js'
+  import { createState, STATE_CONTEXT_KEY } from './state.svelte.js'
+
+  type Props = { children: Snippet; onCollapseChange: InspectOptions['onCollapseChange'] }
+
+  const { children, onCollapseChange }: Props = $props()
+  const initState = $state({})
+  const inspectState = createState(initState, (state) => onCollapseChange?.(state))
+
+  setContext(STATE_CONTEXT_KEY, inspectState)
+</script>
+
+{@render children()}
