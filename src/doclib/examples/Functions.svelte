40384e0a965216de31da1a22a5258303fc6fb2bc
ampled <korsneseirik@gmail.com>
2025-04-25 22:02:17 +0200

    ensure multiline functions in example

--- src/doclib/examples/Functions.svelte
+++ src/doclib/examples/Functions.svelte
@@ -5,60 +5,51 @@
 
   getContext<SvelteMap<string, string>>('toc')?.set('Functions', 'functions')
 
-  function* fibonacci() {
-    let current = 1
-    let next = 1
-    while (true) {
-      yield current
-      ;[current, next] = [next, current + next]
-    }
-  }
-
-  async function* suspensefulFibonacci() {
-    let current = 1
-    let next = 1
-    while (true) {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(undefined)
-        }, 1000)
-      })
-      yield current
-      ;[current, next] = [next, current + next]
-    }
-  }
-
   const arrowFunction = eval(`(num) => num * 2`)
   const asyncFn = eval(`async (num) => num * 2`)
   const someFunction = eval(`(function someFunction(some, thing) {
-        if (!some) return thing
-        const obj = {
-          some: thing,
-          thing: some,
-          [Symbol('oh')]: 'doodle',
-        }
+    if (!some) return thing
+    const obj = {
+      some: thing,
+      thing: some,
+      [Symbol('oh')]: 'doodle',
+    }
 
-        try {
-          Math.random()
-        } catch {
-          const { log } = console
-          log('oh no')
-          log(obj)
-        }
-        return some + ' ' + thing
-      })`)
+    try {
+      Math.random()
+    } catch {
+      const { log } = console
+      log('oh no')
+      log(obj)
+    }
+    return some + ' ' + thing
+  })`)
+  const generator = eval(`(function* fibonacci() {
+    let current = 1
+    let next = 1
+    while (true) {
+      yield current
+      ;[current, next] = [next, current + next]
+    }
+  })`)
+  const asyncGenerator = eval(`(async function* suspensefulFibonacci() {
+  let current = 1
+  let next = 1
+  while (true) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(undefined)
+      }, 1000)
+    })
+    yield current
+    ;[current, next] = [next, current + next]
+  }
+})`)
 </script>
 
 <div class="flex col">
   <h3 id="functions">Functions</h3>
   <p>Display bodies of functions with <code>hljs</code> syntax highlighting.</p>
 
-  <Inspect.Values.Expand0
-    {arrowFunction}
-    {asyncFn}
-    {someFunction}
-    generator={fibonacci}
-    asyncGenerator={suspensefulFibonacci}
-    suspensefulFibonacciTest={suspensefulFibonacci()}
-  />
+  <Inspect.Values.Expand0 {arrowFunction} {asyncFn} {someFunction} {generator} {asyncGenerator} />
 </div>
