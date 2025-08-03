78270e5f7d6b132b50a347d3489eb54d041bb4b8
ampled <korsneseirik@gmail.com>
2025-08-03 15:09:11 +0200

    fix test

--- tests/gettersetter.svelte.test.ts
+++ tests/gettersetter.svelte.test.ts
@@ -22,14 +22,14 @@ test('it has interactive elements for getters and setters', async () => {
 
   const { unmount, user } = renderInspect({ value, expandAll: true, name: 'numberValue' })
 
-  const key = screen.queryAllByTestId('key')[1]
+  const key = screen.queryAllByTestId('key')[2]
 
   // getters / setters get key prefixes
   expect(key).toHaveTextContent('set|get current')
 
   const getButton = screen.queryAllByRole('button', { name: 'get' })[0]
 
-  let getterValueElement = screen.queryAllByTestId('value')[0]
+  let getterValueElement = screen.queryAllByTestId('value')[1]
 
   expect(getterValueElement).toHaveTextContent('get current() { return this.value; }')
 
